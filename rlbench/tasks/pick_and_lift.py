b16beb9c4fcbd40d9144f0f3070c34ecdb01d625
Justin-Yuan <justin.zcyuan@gmail.com>
2021-04-14 19:31:16 -0400

    fixed error in task design .ttt file, added target location to pick_and_lift task

--- rlbench/tasks/pick_and_lift.py
+++ rlbench/tasks/pick_and_lift.py
@@ -13,8 +13,8 @@ class PickAndLift(Task):
     def init_task(self) -> None:
         self.target_block = Shape('pick_and_lift_target')
         self.distractors = [
-            Shape('stack_blocks_distractor%d' % i)
-            for i in range(2)]
+            Shape('stack_blocks_distractor%d' % i) for i in range(2)
+        ]
         self.register_graspable_objects([self.target_block])
         self.boundary = SpawnBoundary([Shape('pick_and_lift_boundary')])
         self.success_detector = ProximitySensor('pick_and_lift_success')
@@ -30,28 +30,34 @@ class PickAndLift(Task):
         block_color_name, block_rgb = colors[index]
         self.target_block.set_color(block_rgb)
 
-        color_choices = np.random.choice(
-            list(range(index)) + list(range(index + 1, len(colors))),
-            size=2, replace=False)
+        color_choices = np.random.choice(list(range(index))
+                                         + list(range(index + 1, len(colors))),
+                                         size=2,
+                                         replace=False)
         for i, ob in enumerate(self.distractors):
             name, rgb = colors[color_choices[int(i)]]
             ob.set_color(rgb)
 
         self.boundary.clear()
-        self.boundary.sample(
-            self.success_detector, min_rotation=(0.0, 0.0, 0.0),
-            max_rotation=(0.0, 0.0, 0.0))
+        self.boundary.sample(self.success_detector,
+                             min_rotation=(0.0, 0.0, 0.0),
+                             max_rotation=(0.0, 0.0, 0.0))
         for block in [self.target_block] + self.distractors:
             self.boundary.sample(block, min_distance=0.1)
 
-        return ['pick up the %s block and lift it up to the target' %
-                block_color_name,
-                'grasp the %s block to the target' % block_color_name,
-                'lift the %s block up to the target' % block_color_name]
+        return [
+            'pick up the %s block and lift it up to the target'
+            % block_color_name,
+            'grasp the %s block to the target' % block_color_name,
+            'lift the %s block up to the target' % block_color_name
+        ]
 
     def variation_count(self) -> int:
         return len(colors)
-    
-    def get_low_dim_state(self) -> dict:
-        return [self.target_block.get_pose(), self.distractors[0].get_pose(), self.distractors[1].get_pose()]
 
+    def get_low_dim_state(self) -> dict:
+        return [
+            self.target_block.get_pose(), self.distractors[0].get_pose(),
+            self.distractors[1].get_pose(),
+            self.success_detector.get_pose()
+        ]
